692fff5c641fbeb1563d219b4918ab8146b59b94
hmu0428 <hmu0428@gmail.com>
2025-03-14 17:56:07 +0900

    초기설정

---
+++ src/test/java/sm/ac/CdprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package sm.ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
