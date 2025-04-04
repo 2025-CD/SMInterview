8e8ced168f4607b3824c9c5cc752010de8d559c4
hmu0428 <hmu0428@gmail.com>
2025-04-04 23:44:56 +0900

    a

--- src/main/java/sm/ac/config/SecurityConfig.java
+++ src/main/java/sm/ac/config/SecurityConfig.java
@@ -5,11 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-import org.springframework.security.oauth2.client.OAuth2AuthorizationRequest;
-import java.util.function.Consumer;
-import org.springframework.security.config.Customizer;
 
 @Configuration
 @EnableWebSecurity
@@ -27,9 +23,7 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/user")
                 )
                 .oauth2Login(oauth2 -> oauth2
-                        .authorizationEndpoint(authorization -> authorization
-                                .customizers(Customizer.withDefaults())
-                        )
+                        .loginPage("/login")
                         .defaultSuccessUrl("/user")
                 )
                 .logout(logout -> logout
@@ -38,13 +32,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public Consumer<OAuth2AuthorizationRequest> kakaoAuthorizationRequestCustomizer(HandlerMappingIntrospector introspector) {
-        return request -> {
-            MvcRequestMatcher requestMatcher = new MvcRequestMatcher(introspector, "/oauth2/authorization/kakao");
-            if (requestMatcher.matches(request.getAttribute(MvcRequestMatcher.class.getName()))) {
-                request.attributes(attributes -> attributes.put("registrationId", "kakao"));
-            }
-        };
-    }
 }
